fa3c69bf4faf177d3cefb8c5d71e725eeef37285
BH0 <BH0@users.noreply.github.com>
2021-08-25 11:02:49 +0100

    Add files via upload

---
+++ .github/workflows/wakeup.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://xshss.sse.codesandbox.io/lyrics?artist=disturbed&song=stricken');
+  await page.screenshot({ path: 'example.png' });
+  await page.waitForNavigation({
+    waitUntil: 'networkidle0',
+  });
+  await browser.close();
+})();
